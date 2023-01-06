59b1ff1da56c2deace18a6415f1affbf478a717a
Siddharth verma <74046564+sidharthviz@users.noreply.github.com>
2023-01-07 02:41:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const emojiPedia = {
+  "🙂": "Happy",
+  "😞": "Grief",
+  "🤔": "Thoughful",
+  "😊": "Smiling",
+  "❤️": "Like"
+};
+
+var emojiweKnow = Object.keys(emojiPedia);
+
+export default function App() {
+  const [meaning, setMeaning] = useState("");
+
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+    var meaning = emojiPedia[userInput];
+
+    if (meaning === undefined) {
+      meaning = "oops! This is out of dictionary 😓";
+    }
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiPedia[emoji];
+    setMeaning(meaning);
+  }
+
+  return (
+    <div className="App">
+      <h1>Watch your Emoji 👻</h1>
+      <input
+        className="text"
+        placeholder="Put your emoji"
+        onChange={emojiInputHandler}
+      ></input>
+
+      <h2>{meaning}</h2>
+      <h3>Emoji&apos;s We Know ~</h3>
+      {emojiweKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "1rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {" "}
+            {emoji}{" "}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
